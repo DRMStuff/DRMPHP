3dac5a41f65c5f8d06c03ee8790d6c14d1de02f4
root <office@darklinux.uk>
2023-06-05 08:44:36 +0300

    update

---
+++ panel/_footer.php
@@ -0,0 +1,13 @@
+<?php
+$version = "v0.1b";
+?>
+
+<footer class="footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6">
+        <script>document.write(new Date().getFullYear())</script> Â© DRMPHP <?php echo $version; ?>v.
+      </div>
+    </div>
+  </div>
+</footer>
